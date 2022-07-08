0ae29d660272feee0bd9519f277022d4662ba22f
Isaac <isaacsummers314@gmail.com>
2022-07-08 15:17:03 -0400

    added pandas dataframe for fruit smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 from codecs import ignore_errors
 
 import streamlit
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 
 streamlit.header('Breakfast Menu')
 streamlit.text('😅Omega 3 & Blueberry Oatmeal')
@@ -8,3 +12,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
