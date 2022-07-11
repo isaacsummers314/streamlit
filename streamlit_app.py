a44caa03c04de84322fdcb80b6eb9c7698052ec3
Isaac <isaacsummers314@gmail.com>
2022-07-11 13:45:50 -0400

    fixed second choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ streamlit.dataframe(my_data_rows)
 another_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', another_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + another_choice)
+another_response = requests.get("https://fruityvice.com/api/fruit/" + another_choice)
