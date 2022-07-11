3f5ea50dcaf9986af3239a8faa24b1e58b292d7b
Isaac <isaacsummers314@gmail.com>
2022-07-11 14:06:15 -0400

    sourcery

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,18 +28,15 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-    if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-    else:
+    if fruit_choice := streamlit.text_input('What fruit would you like information about?'):
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
+    else:
+        streamlit.error("Please select a fruit to get information.")
 except URLError as e:
     streamlit.error()
     
-
-
 #### End of Fruityvice section
 
 streamlit.stop()
