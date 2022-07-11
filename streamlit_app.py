b7437d73f066020ba160aaeaafb5098eb94e50b6
Isaac <isaacsummers314@gmail.com>
2022-07-11 13:48:04 -0400

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-another_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', another_choice)
+add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'Jackfruit')
+streamlit.write('The user entered', add_my_fruit)
 
-another_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+another_response = requests.get("https://fruityvice.com/api/fruit/" + "Jackfruit")
