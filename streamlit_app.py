0df6c32d0e3775f9f266a11f224015b98242c2b3
Isaac <isaacsummers314@gmail.com>
2022-07-11 12:21:18 -0400

    convert requests get to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import streamlit
 import pandas as pd
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -25,6 +24,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected] if fruits_selected else my_f
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.text(fruityvice_response)
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
 
 
