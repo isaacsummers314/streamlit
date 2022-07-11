56fa4c54ef52fa7193654b88c36736ee661874e9
Isaac <isaacsummers314@gmail.com>
2022-07-11 14:01:25 -0400

    streamlit try except block

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected] if fruits_selected else my_f
 streamlit.dataframe(fruits_to_show)
 
 
-
+# New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
+    
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
 #### End of Fruityvice section
 
