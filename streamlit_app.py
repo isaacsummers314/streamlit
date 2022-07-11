6d25d6eb3d7188d945c6d433fc4790b9f4da7f19
Isaac <isaacsummers314@gmail.com>
2022-07-11 13:52:11 -0400

    execute write fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'Jackfruit')
-streamlit.write('The user entered', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 another_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
